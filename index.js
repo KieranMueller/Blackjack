ac724ba8eb98df4766a048916f5ec18fd3ab93fc
KieranMueller <121121198+KieranMueller@users.noreply.github.com>
2023-01-06 00:31:44 -0600

    Update index.js

--- index.js
+++ index.js
@@ -15,9 +15,17 @@ let sumMessage = document.getElementById("sum");
 let displayCards = document.querySelector(".cards");
 let actionButtonOne = document.querySelector(".actionButtonOne");
 let actionButtonTwo = document.querySelector(".actionButtonTwo");
+let actionButtonThree = document.querySelector(".actionButtonThree");
 function start(){
     renderGame()
 }
+// Player Object
+let player = document.querySelector(".playerObject");
+playerObject = {
+    name: "Kieran",
+    chips: 600
+}
+
 
 //Functions
 
@@ -27,18 +35,22 @@ function start(){
 //General Parameters
 function start(){
     isAlive = true;
+    player.textContent = playerObject.name + ": $" + playerObject.chips;
     let firstCard = randomNumber();
     let secondCard = randomNumber();
     cards = [firstCard, secondCard];
+    for (let i = 0; i<cards.length; i++){
+        displayCards.textContent += " " + cards[i] + " ";
+    }
     sum = cards[0] + cards[1];
     renderGame()
 }
 function renderGame(){
     sumMessage.textContent = "Sum: ...";
     setTimeout(function(){sumMessage.textContent = "Sum: " + sum},500);
-    displayCards.textContent = ("Cards: " + cards[0] + ", " + cards[1]);
     {
         if (sum < 21) {
+            isAlive = true;
             message = ("Hit?");
             setTimeout(function() {actionButtonOne.textContent = "Hit"},500)
             setTimeout(function() {actionButtonTwo.textContent = "Stand"},500)
@@ -47,6 +59,7 @@ function renderGame(){
             
         }
         if (sum === 21) {
+            isAlive = false;
             message = ("BLACKJACK!");
             hasBlackjack = true;
             displayMessage.textContent = "Checking..."
@@ -60,26 +73,41 @@ function renderGame(){
             setTimeout(function() {displayMessage.textContent = message},500)
             
         }
+          
+              if (!isAlive){
+                setTimeout(function(){actionButtonThree.textContent = "Reset"},500)
+                setTimeout(function(){actionButtonOne.textContent = " "},500)
+                setTimeout(function(){actionButtonTwo.textContent = " "},500)
+                }
     };
 }
 
-function hit(){
+let hit = function hit(){
+    if(isAlive){
     let newCard = randomNumber();
     sum += newCard;
     cards.push(newCard);
-    console.log(cards);
     renderGame();
     displayMessage.textContent = "Fetching new card...";
-    setTimeout(function(){displayCards.textContent += ", " + newCard},500)
+   setTimeout(function(){displayCards.textContent +=  newCard + " "},500)
+    }
+}
+function reset(){
+    if(!isAlive)
+    document.location.reload();
 }
 
 /* To-Do
+Fixes
+- i can hit play blackjack button multiple times
+
 1. let Ace = 1 or 11
 2. make actual cards
 3. create a dealer/bank
 4. create chips/gambling system(add bet button etc. split, raise, double down)
 5. give functionality to stand button
 6. add age verification
+7. add funny stuff (add tik-tok explosion video to screen when blackjack (override muted autoplay?))
 
 
 */
