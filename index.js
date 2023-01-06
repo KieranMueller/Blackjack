1463f0da8b2af945fa5d77a205ef23a4a6113d7e
KieranMueller <121121198+KieranMueller@users.noreply.github.com>
2023-01-05 19:33:15 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,65 +1,85 @@
+function randomNumber(){
+    let randomNumber = Math.floor((Math.random()*13)+1)
+    if (randomNumber === 1) return 11;
+    if(randomNumber > 10) return 10;
+    return randomNumber
+};
 // Variables
-
-let firstCard = 11;
-let secondCard = 8;
-let cards = [firstCard, secondCard];
-let sum = cards[0] + cards[1];
-let age = 20;
+let cards = [];
+let sum = 0;
 let hasBlackjack = false;
-isAlive = true;
+let isAlive = false;
 let message = " ";
 let displayMessage = document.getElementById("message");
 let sumMessage = document.getElementById("sum");
 let displayCards = document.querySelector(".cards");
 let actionButtonOne = document.querySelector(".actionButtonOne");
 let actionButtonTwo = document.querySelector(".actionButtonTwo");
-
+function start(){
+    renderGame()
+}
 
 //Functions
 
 //Action Functions
 
-function hit(){
-    let card = 1;
-    sum += card;
-    start();
-    displayMessage.textContent = "Fetching new card...";
-    setTimeout(function(){displayCards.textContent += ", " + card},500)
-}
 
 //General Parameters
 function start(){
+    isAlive = true;
+    let firstCard = randomNumber();
+    let secondCard = randomNumber();
+    cards = [firstCard, secondCard];
+    sum = cards[0] + cards[1];
+    renderGame()
+}
+function renderGame(){
     sumMessage.textContent = "Sum: ...";
     setTimeout(function(){sumMessage.textContent = "Sum: " + sum},500);
     displayCards.textContent = ("Cards: " + cards[0] + ", " + cards[1]);
-{
-if (sum < 21) {
-    message = ("Hit?");
-    setTimeout(function() {actionButtonOne.textContent = "Hit"},500)
-    setTimeout(function() {actionButtonTwo.textContent = "Stand"},500)
-    displayMessage.textContent = "Checking..."
-    setTimeout(function() {displayMessage.textContent = message},500)
-    
+    {
+        if (sum < 21) {
+            message = ("Hit?");
+            setTimeout(function() {actionButtonOne.textContent = "Hit"},500)
+            setTimeout(function() {actionButtonTwo.textContent = "Stand"},500)
+            displayMessage.textContent = "Checking..."
+            setTimeout(function() {displayMessage.textContent = message},500)
+            
+        }
+        if (sum === 21) {
+            message = ("BLACKJACK!");
+            hasBlackjack = true;
+            displayMessage.textContent = "Checking..."
+            setTimeout(function() {displayMessage.textContent = message},500)
+            
+        }
+        if (sum > 21) {
+            message = ("Bust");
+            isAlive = false;
+            displayMessage.textContent = "Checking..."
+            setTimeout(function() {displayMessage.textContent = message},500)
+            
+        }
+    };
 }
-if (sum === 21) {
-    message = ("BLACKJACK!");
-    hasBlackjack = true;
-    displayMessage.textContent = "Checking..."
-    setTimeout(function() {displayMessage.textContent = message},500)
 
+function hit(){
+    let newCard = randomNumber();
+    sum += newCard;
+    cards.push(newCard);
+    console.log(cards);
+    renderGame();
+    displayMessage.textContent = "Fetching new card...";
+    setTimeout(function(){displayCards.textContent += ", " + newCard},500)
 }
-if (sum > 21) {
-    message = ("Bust");
-    isAlive = false;
-    displayMessage.textContent = "Checking..."
-    setTimeout(function() {displayMessage.textContent = message},500)
 
-}
-};
-}
-// Age Thing
-let ageMessage = " ";
-{
-    if (age>=21) ageMessage = ("Welcome!")
-    if (age<21) ageMessage = ("We noticed you're under 21... Just don't tell your parents.")
-}
+/* To-Do
+1. let Ace = 1 or 11
+2. make actual cards
+3. create a dealer/bank
+4. create chips/gambling system(add bet button etc. split, raise, double down)
+5. give functionality to stand button
+6. add age verification
+
+
+*/
